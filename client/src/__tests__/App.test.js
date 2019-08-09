0ed5af8d09f11ccb932487c63e7fd9df029b3968
Daxo <17711952+Daxo@users.noreply.github.com>
2019-08-09 17:29:05 -0500

    Added higher level files

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import {render, fireEvent, cleanup, waitForElement} from 'react-testing-library'
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from 'react-testing-library';
 
 // this adds custom jest matchers from jest-dom
-import 'jest-dom/extend-expect'
+import 'jest-dom/extend-expect';
 import App from '../App';
 
 afterEach(cleanup);
 
-
 it('CheckboxWithLabel changes the text after click', async () => {
-    const { getByText } = render(<App/>,);
+  const { getByText } = render(<App />);
 
-    const dolphin = await waitForElement(() => getByText(/dolphin/i),)
+  const dolphin = await waitForElement(() => getByText(/dolphin/i));
 
-    expect(dolphin).toBeTruthy();
+  expect(dolphin).toBeTruthy();
 });
