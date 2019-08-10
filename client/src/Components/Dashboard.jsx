1f8e76797c9e628e5e907b0fafaea9ac5ea527a7
Garrett Welson <garrettwelson@gmail.com>
2019-08-10 17:01:54 -0500

    Added category select field

--- client/src/Components/Dashboard.jsx
+++ client/src/Components/Dashboard.jsx
@@ -3,24 +3,29 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Tooltip from '@material-ui/core/Tooltip';
+import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import MomentUtils from '@date-io/moment';
-import { makeStyles } from '@material-ui/core/styles';
 import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider
 } from '@material-ui/pickers';
-
-const useStyles = makeStyles(() => {
-  root: {
-    flexGrow: 1;
-  }
-});
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       accountData: {},
+      categories: [
+        'rent',
+        'groceries',
+        'transportation',
+        'bills',
+        'clothes',
+        'going out',
+        'household expenses'
+      ],
       netBalance: 0,
       inputAmount: 0,
       inputCategory: '',
@@ -28,6 +33,8 @@ export default class Dashboard extends Component {
       inputDate: new Date()
     };
     this.handleDateInput = this.handleDateInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleCategoryInput = this.handleCategoryInput.bind(this);
   }
 
   componentDidMount() {
@@ -45,22 +52,29 @@ export default class Dashboard extends Component {
       inputDate: value
     });
   }
+
+  handleAmountInput(value) {
+    this.setState({
+      inputAmount: value
+    });
+  }
+
+  handleCategoryInput(value) {
+    this.setState({
+      inputCategory: value
+    });
+  }
+
   render() {
-    const themes = useStyles();
     return (
-      <div className={themes.root}>
-        <Grid
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="center"
-        >
+      <div style={styles.root}>
+        <Grid container direction="column" justify="center" alignItems="center">
           <Paper>
             <Typography variant="h1" gutterBottom>
               Hello, {this.state.accountData.firstName}!
             </Typography>
             <Tooltip
-              placement="bottom"
+              placement="top"
               title="Safe to spend balance: bank accounts less credit card debt"
             >
               <Typography variant="h2">
@@ -69,19 +83,54 @@ export default class Dashboard extends Component {
             </Tooltip>
           </Paper>
           <Paper>
-            <Typography variant="h4">Add a transaction</Typography>
-            <MuiPickersUtilsProvider utils={MomentUtils}>
-              <KeyboardDatePicker
-                variant="inline"
-                format="MM/DD/YYYY"
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+            >
+              <Typography variant="h4">Add a transaction</Typography>
+              <MuiPickersUtilsProvider utils={MomentUtils}>
+                <KeyboardDatePicker
+                  variant="inline"
+                  format="MM/DD/YYYY"
+                  margin="normal"
+                  value={this.state.inputDate}
+                  onChange={this.handleDateInput}
+                />
+              </MuiPickersUtilsProvider>
+              <TextField
+                id="amount"
+                label="amount"
+                value={this.state.inputAmount}
+                onChange={this.handleAmountInput}
                 margin="normal"
-                value={this.state.inputDate}
-                onChange={this.handleDateInput}
               />
-            </MuiPickersUtilsProvider>
+              <Select
+                value={this.state.inputCategory}
+                onChange={this.handleCategoryInput}
+                inputProps={{
+                  name: 'category'
+                }}
+              >
+                {this.state.categories.map((category, i) => {
+                  return (
+                    <MenuItem key={`categoryInput_${i}`} value={category}>
+                      {category}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </Grid>
           </Paper>
         </Grid>
       </div>
     );
   }
 }
+
+const styles = {
+  root: {
+    flexGrow: 1
+  }
+};
