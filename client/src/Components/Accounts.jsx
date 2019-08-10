f038bdf80a9619d8c90a91fe40ff084df719b3c0
Doris Hui <dorishui225@gmail.com>
2019-08-10 15:26:23 -0500

    added button for add accounts

--- client/src/Components/Accounts.jsx
+++ client/src/Components/Accounts.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
+import Chip from '@material-ui/core/Chip';
+import Avatar from '@material-ui/core/Avatar';
 import AddIcon from '@material-ui/icons/Add';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 export default class Accounts extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.handleAddAccount = this.handleAddAccount.bind(this);
   }
+
+  handleAddAccount() {}
 
   render() {
     return (
@@ -17,13 +26,49 @@ export default class Accounts extends React.Component {
         <h1>Accounts</h1>
         <Grid container spacing={3}>
           <Grid item xs={6}>
-            <Paper>xs=6</Paper>
-            <Fab color="primary" aria-label="add">
-              <AddIcon />
-            </Fab>
+            <Paper>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Account</TableCell>
+                    <TableCell align="center">Balance</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Account Name</TableCell>
+                    <TableCell>$ Balance.00</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Paper>
+            <Chip
+              avatar={
+                <Avatar>
+                  <AddIcon />
+                </Avatar>
+              }
+              label="Add an account"
+              color="primary"
+              onClick={this.handleAddAccount}
+              variant="outlined"
+            />
           </Grid>
           <Grid item xs={6}>
-            <Paper>xs=6</Paper>
+            <Paper>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="center">Transactions</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Transaction 1</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Paper>
           </Grid>
         </Grid>
       </div>
