93037a1d4dac940bb575d487d199cc70b115eec4
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-08-10 12:25:15 -0500

    added test paragraphs

--- client/src/Components/ProfilePage.jsx
+++ client/src/Components/ProfilePage.jsx
@@ -5,16 +5,21 @@ export default class ProfilePage extends React.Component {
         super(props)
 
         this.state = {
-            email: '',
-            firstName: '',
-            lastName: '',
-
+            email: 'email',
+            firstName: 'name1',
+            lastName: 'name2',
+            notifications: [],
         }
     }
 
     render () {
         return (
-            <h1>Profile Page</h1>
+            <>
+                <h1>Profile Page</h1>
+                <p>{this.state.firstName}</p>
+                <p>{this.state.lastName}</p>
+                <p>{this.state.email}</p>
+            </>
         )
     }
 }
