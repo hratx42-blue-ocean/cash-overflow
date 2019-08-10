4b65865a89b86330daeb225bc5872dc6b9885794
Brandon Leafman <17711952+bleafman@users.noreply.github.com>
2019-08-10 15:39:43 -0500

    Routing v1 (#61)
    * Made sample page files to mock routes
    
    * Updated server to work with React Routes
    
    * Reconfigured webpack-dev-server to proxy server
    
    * Added sourcemap for faster rebuilds during development
    
    * Updated tests to work with react-router

---
+++ client/src/Components/ProfilePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  }
+}));
+
+function ProfilePage() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Grid item />
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>ProfilePage</Paper>
+      </Grid>
+      <Grid item />
+    </>
+  );
+}
+
+export default ProfilePage;
