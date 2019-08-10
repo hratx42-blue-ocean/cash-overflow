82506e8ffad42cd1f43546f3453c0d440b08f992
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-08-10 15:53:11 -0500

    field input updates state of component

--- client/src/Components/ProfilePage.jsx
+++ client/src/Components/ProfilePage.jsx
@@ -1,45 +1,9 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputBase from '@material-ui/core/InputBase';
-
-const BootstrapInput = withStyles(theme => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3)
-    }
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 26px 10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"'
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-    }
-  }
-}))(InputBase);
+import Input from '@material-ui/core/Input';
 
 export default class ProfilePage extends React.Component {
   constructor(props) {
@@ -51,12 +15,14 @@ export default class ProfilePage extends React.Component {
       firstName: 'name1',
       firstNameIsHidden: true,
       lastName: 'name2',
-      lastNameIsHidden: true
+      lastNameIsHidden: true,
+      input: ''
     };
 
     this.emailButtonHandler = this.emailButtonHandler.bind(this);
     this.firstNameButtonHandler = this.firstNameButtonHandler.bind(this);
     this.lastNameButtonHandler = this.lastNameButtonHandler.bind(this);
+    this.handleInput = this.handleInput.bind(this);
   }
 
   emailButtonHandler(e) {
@@ -76,6 +42,11 @@ export default class ProfilePage extends React.Component {
     });
   }
 
+  handleInput(e) {
+    let input = e.target.value;
+    this.setState({ input });
+  }
+
   render() {
     return (
       <Grid justify="center" alignItems="center">
@@ -84,27 +55,36 @@ export default class ProfilePage extends React.Component {
         <p>
           {this.state.firstName}
           <Button onClick={this.firstNameButtonHandler}>Edit</Button>
-          {!this.state.firstNameIsHidden && <Child />}
+          {!this.state.firstNameIsHidden && (
+            <Child handleInput={this.handleInput} />
+          )}
         </p>
         <p>
           {this.state.lastName}
           <Button onClick={this.lastNameButtonHandler}>Edit</Button>
-          {!this.state.lastNameIsHidden && <Child />}
+          {!this.state.lastNameIsHidden && (
+            <Child handleInput={this.handleInput} />
+          )}
         </p>
         <p>
           {this.state.email}
           <Button onClick={this.emailButtonHandler}>Edit</Button>
-          {!this.state.emailIsHidden && <Child />}
+          {!this.state.emailIsHidden && (
+            <Child handleInput={this.handleInput} />
+          )}
         </p>
       </Grid>
     );
   }
 }
 
-const Child = () => (
-  <form autoComplete="off">
-    <FormControl>
-      <BootstrapInput />
-    </FormControl>
-  </form>
-);
+const Child = props =>
+  (Child.propTypes = {
+    inputHandler: PropTypes.func
+  })(
+    <form autoComplete="off">
+      <FormControl>
+        <Input onChange={props.handleInput} />
+      </FormControl>
+    </form>
+  );
