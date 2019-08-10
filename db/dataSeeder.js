088062ccd7c7c8481997c01b6d7f04123406c9aa
Garrett Welson <garrettwelson@gmail.com>
2019-08-10 12:41:06 -0500

    Add transaction generation to data seeder script

--- db/dataSeeder.js
+++ db/dataSeeder.js
@@ -1,6 +1,7 @@
 const faker = require('faker');
 
 module.exports.createData = () => {
+  // create basic object structure
   const dummyObject = {
     userID: faker.random.uuid(),
     email: faker.internet.email(),
@@ -11,14 +12,46 @@ module.exports.createData = () => {
         name: faker.company.companyName(),
         type: faker.finance.accountName(),
         transactions: {
-          
+          2019: {
+            8: []
+          }
         }
-      },
-      {}.
+      }
     ],
     recurringTransactions: [],
     budgetCategories: [],
     notifications: []
   };
+  const categories = [
+    'rent',
+    'groceries',
+    'transportation',
+    'bills',
+    'clothes',
+    'going out',
+    'household expenses'
+  ];
+
+  // seed August 2019 with 40 transactions
+  for (let i = 0; i < 40; i++) {
+    dummyObject.accounts[0].transactions[2019][8].push({
+      id: faker.random.uuid(),
+      amount: faker.finance.amount(),
+      category: categories[Math.floor(Math.random() * categories.length)],
+      date: faker.date.between('2019-08-01', '2019-08-31'),
+      payee: faker.company.companyName(),
+      recurring: faker.random.boolean()
+    });
+  }
+
+  // generate 5 recurring transactions
+  for (let i = 0; i < 5; i++) {
+    dummyObject.recurringTransactions.push({
+      id: faker.random.uuid(),
+      amount: faker.finance.amount(),
+      category: categories[Math.floor(Math.random() * categories.length)],
+      payee: faker.company.companyName()
+    });
+  }
   return dummyObject;
 };
