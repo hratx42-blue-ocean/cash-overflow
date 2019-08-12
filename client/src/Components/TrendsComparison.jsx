d7a7b4ac2c7c23cd519dd08d327915b481406896
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-12 10:32:35 -0500

    Fix linting issues

--- client/src/Components/TrendsComparison.jsx
+++ client/src/Components/TrendsComparison.jsx
@@ -3,24 +3,14 @@ import { Bar } from 'react-chartjs-2';
 
 // test data for display
 const data = {
-  labels: [
-    'Red',
-    'Green',
-    'Yellow',
-  ],
-  datasets: [{
-    data: [300, 50, 100],
-    backgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56',
-    ],
-    hoverBackgroundColor: [
-      '#FF6384',
-      '#36A2EB',
-      '#FFCE56',
-    ],
-  }],
+  labels: ['Red', 'Green', 'Yellow'],
+  datasets: [
+    {
+      data: [300, 50, 100],
+      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+    }
+  ]
 };
 
 const TrendsComparison = () => {
