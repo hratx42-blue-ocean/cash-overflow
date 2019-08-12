57e4afa8e3cd1979cc1605f749411b14c4e6efdb
Mitchell Dill <malexanderdill@gmail.com>
2019-08-12 16:25:48 -0500

    Seed DB with 5 fake users, create .env
    Co-authored-by: kbench09 <kevinrbench@gmail.com>

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -1,31 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+require('dotenv').config();
+const dataSeeder = require('./dataSeeder.js');
 
-const uri =
-  'mongodb+srv://greenoceanatx:hackatx42@greenocean-naa2k.gcp.mongodb.net/greenOcean?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@greenocean-naa2k.gcp.mongodb.net/greenOcean?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
-
-// client.connect(err => {
-//   if (err) {
-//     throw err;
-//   } else {
-//     const collection = client.db('greenOcean').collection('userData');
-//     // perform actions on the collection object
-//     const cursor = collection
-//       .find({ name: 'testObject' })
-//       .toArray((err, doc) => {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           assert.equal(null, err);
-//           assert.equal(1, doc.length);
-//           console.log(null, doc);
-//         }
-//       });
-//     console.log(cursor);
-//     client.close();
-//   }
-// });
 
 const getUserData = async userEmail => {
   try {
@@ -43,6 +22,22 @@ const getUserData = async userEmail => {
   }
 };
 
+const seedFakeUserData = async () => {
+  try {
+    await client.connect();
+    const collection = await client.db('greenOcean').collection('userData');
+    const bulk = collection.initializeUnorderedBulkOp();
+    for (let i = 0; i < 5; i++) {
+      const fakeUserData = dataSeeder.createData();
+      bulk.insert(fakeUserData);
+    }
+    await bulk.execute();
+  } finally {
+    client.close();
+  }
+};
+
+// seedFakeUserData();
 getUserData('test@gmail.com');
 
 module.exports = { getUserData };
