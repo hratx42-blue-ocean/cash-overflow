07f292b04952d8ca2a8189e2c9c8054dbdb1a19b
Mitchell Dill <malexanderdill@gmail.com>
2019-08-12 14:46:12 -0500

    Test connection using assert

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -1,12 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
 const uri =
   'mongodb+srv://greenoceanatx:hackatx42@greenocean-naa2k.gcp.mongodb.net/greenOcean?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true });
+
 client.connect(err => {
-  const collection = client.db('greenOcean').collection('userData');
-  // perform actions on the collection object
-  const cursor = collection.find({});
-  console.log(cursor);
-  console.log(err);
-  client.close();
+  if (err) {
+    throw err;
+  } else {
+    const collection = client.db('greenOcean').collection('userData');
+    // perform actions on the collection object
+    const cursor = collection
+      .find({ name: 'testObject' })
+      .toArray((err, doc) => {
+        if (err) {
+          console.log(err);
+        } else {
+          assert.equal(null, err);
+          assert.equal(1, doc.length);
+          console.log(null, doc);
+        }
+      });
+    console.log(cursor);
+    client.close();
+  }
 });
