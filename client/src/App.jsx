826d6212c463a9de74d78d6cf1eb3f92798a487c
Garrett Welson <garrettwelson@gmail.com>
2019-08-13 11:28:40 -0500

    staging all changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+// Routing
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import PrivateRoute from './Components/PrivateRoute.jsx';
 
 // Material Components
 import Container from '@material-ui/core/Container';
 
-// import './App.css';
 // import Budget from './Components/BudgetPage.jsx';
 import fakeData from '../../db/dataSeeder.js';
 
@@ -41,56 +42,58 @@ export default class App extends Component {
     const { accountData, budgetCategories } = this.state;
     return (
       <div className="app">
-        <Header />
-        <Container maxWidth="sm">
-          <h1>Welcome to Green Ocean!</h1>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={props => (
-                <LandingPage {...props} accountData={accountData} />
-              )}
-            />
-            <Route
-              path="/accounts"
-              render={props => (
-                <AccountsPage {...props} accountData={accountData} />
-              )}
-            />
-            <Route
-              path="/budget"
-              render={props => (
-                <BudgetPage {...props} categories={budgetCategories} />
-              )}
-            />
-            <Route
-              path="/dashboard"
-              render={props => (
-                <DashboardPage {...props} accountData={accountData} />
-              )}
-            />
-            <Route
-              path="/login"
-              render={props => (
-                <LoginPage {...props} accountData={accountData} />
-              )}
-            />
-            <Route
-              path="/profile"
-              render={props => (
-                <ProfilePage {...props} accountData={accountData} />
-              )}
-            />
-            <Route
-              path="/trends"
-              render={props => (
-                <TrendsPage {...props} accountData={accountData} />
-              )}
-            />
-            <Route component={ErrorPage} />
-          </Switch>
-        </Container>
+        <BrowserRouter>
+          <Header />
+          <Container maxWidth="sm">
+            <h1>Welcome to Green Ocean!</h1>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <LandingPage {...props} accountData={accountData} />
+                )}
+              />
+              <Route
+                path="/accounts"
+                render={props => (
+                  <AccountsPage {...props} accountData={accountData} />
+                )}
+              />
+              <Route
+                path="/budget"
+                render={props => (
+                  <BudgetPage {...props} accountData={accountData} />
+                )}
+              />
+              <PrivateRoute
+                path="/dashboard"
+                render={props => (
+                  <DashboardPage {...props} accountData={accountData} />
+                )}
+              />
+              <Route
+                path="/login"
+                render={props => (
+                  <LoginPage {...props} accountData={accountData} />
+                )}
+              />
+              <Route
+                path="/profile"
+                render={props => (
+                  <ProfilePage {...props} accountData={accountData} />
+                )}
+              />
+              <Route
+                path="/trends"
+                render={props => (
+                  <TrendsPage {...props} accountData={accountData} />
+                )}
+              />
+              <Route component={ErrorPage} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
       </div>
     );
   }
