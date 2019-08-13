307ce8f4eec918d3c217e4272a15fc5299f5575c
Daxo <17711952+Daxo@users.noreply.github.com>
2019-08-13 08:55:35 -0500

    Added PrivateRoute component for restricting access to pages that require Auth

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { Auth0Provider } from './react-auth0-wrapper';
-import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
 import config from './auth_config.js';
 // import './index.css';
 
-import AuthNavBar from './Components/AuthNavBar.jsx';
 // A function that routes the user to the right place
 // after login
 const onRedirectCallback = appState => {
@@ -27,9 +24,7 @@ ReactDOM.render(
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}
   >
-    <AuthNavBar />
+    <App />
   </Auth0Provider>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
