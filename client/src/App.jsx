7c8168740cb7f6e765cfa278eff05363a970d8f9
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-14 00:13:39 -0500

    Fix formatting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 // Routing
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import Container from '@material-ui/core/Container';
 import PrivateRoute from './Components/PrivateRoute.jsx';
 import { Auth0Context } from './react-auth0-wrapper';
 // Material Components
-import Container from '@material-ui/core/Container';
 
 // import Budget from './Components/BudgetPage.jsx';
 import fakeData from '../../db/dataSeeder.js';
@@ -20,22 +20,24 @@ import TrendsPage from './Components/TrendsPage.jsx';
 import LoginPage from './Components/LoginPage.jsx';
 import ProfilePage from './Components/ProfilePage.jsx';
 import ErrorPage from './Components/ErrorPage.jsx';
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       budgetCategories: [],
       accountData: {
-        accounts: [{ transactions: { year: { month: [] } } }]
-      }
+        accounts: [{ transactions: { year: { month: [] } } }],
+      },
     };
-    this.api = `http://localhost:8000/api/example`;
+    this.api = 'http://localhost:8000/api/example';
   }
+
   componentDidMount() {
     const data = fakeData.createData();
     this.setState({
       budgetCategories: data.budgetCategories,
-      accountData: data
+      accountData: data,
     });
   }
 
@@ -52,19 +54,19 @@ export default class App extends Component {
               <Route
                 exact
                 path="/"
-                render={props => (
+                render={(props) => (
                   <LandingPage {...props} accountData={accountData} />
                 )}
               />
               <Route
                 path="/accounts"
-                render={props => (
+                render={(props) => (
                   <AccountsPage {...props} accountData={accountData} />
                 )}
               />
               <Route
                 path="/budget"
-                render={props => (
+                render={(props) => (
                   <BudgetPage
                     {...props}
                     allotments={budgetCategories}
@@ -75,7 +77,7 @@ export default class App extends Component {
               />
               <PrivateRoute
                 path="/dashboard"
-                render={props => (
+                render={(props) => (
                   <DashboardPage
                     {...props}
                     accountData={accountData}
@@ -86,19 +88,19 @@ export default class App extends Component {
               />
               <Route
                 path="/login"
-                render={props => (
+                render={(props) => (
                   <LoginPage {...props} accountData={accountData} />
                 )}
               />
               <Route
                 path="/profile"
-                render={props => (
+                render={(props) => (
                   <ProfilePage {...props} accountData={accountData} />
                 )}
               />
               <Route
                 path="/trends"
-                render={props => (
+                render={(props) => (
                   <TrendsPage {...props} accountData={accountData} />
                 )}
               />
