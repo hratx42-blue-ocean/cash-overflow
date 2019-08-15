936b0421ecc34e56ca79880e4dfcad80f17b537d
Daxo <17711952+Daxo@users.noreply.github.com>
2019-08-14 20:42:01 -0500

    Updated tests for LandingPage to be Auth aware

--- client/src/__tests__/LandingPage.test.js
+++ client/src/__tests__/LandingPage.test.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
-
+import { AuthAndMemoryWrapper } from './utils/util.wrappers';
 import LandingPage from '../Components/LandingPage';
 
 describe('Dashboard component --->', () => {
   test('should render without throwing an error', async () => {
-    expect(await shallow(<LandingPage />));
+    expect(
+      await shallow(
+        <AuthAndMemoryWrapper>
+          <LandingPage />
+        </AuthAndMemoryWrapper>
+      )
+    );
   });
 
   test('should mount in a full DOM', async () => {
     expect(
       await mount(
-        <MemoryRouter>
+        <AuthAndMemoryWrapper>
           <LandingPage />
-        </MemoryRouter>,
-      ),
+        </AuthAndMemoryWrapper>
+      )
     );
-  });
-
-  test('should be selectable by class "dashboardPage"', async () => {
-    expect(await shallow(<LandingPage />).is('.landingPage')).toBe(true);
   });
 });
