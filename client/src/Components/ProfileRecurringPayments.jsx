be95325c264973f95b97c959d9c9d215f0375ecc
evelynbinkard <46108359+evelynbinkard@users.noreply.github.com>
2019-08-16 17:23:29 -0500

    Profile recurring payment success message (#176)
    * successful toggle function now must clear transaction options
    
    * toggleSuccessFunction now clears user inputs
    
    * Toggling functionality complete in recurring payment
    
    * start work on creating logic for Alert Payment Card
    
    * adjusted tests to accept recurringTransactions as a prop
    
    * proptypes in AlertBox.jsx required
    
    * trying to rebuild
    
    * refactored ProfilePage to abstract ProfileSetRP Component
    
    * fixed import bug for ProfileRPList
    
    * scrollable paper on ProfilePage that displays currently set Recurring Payments. Still needs ability to delete
    
    * merge conflict adjustment

--- client/src/Components/ProfileRecurringPayments.jsx
+++ client/src/Components/ProfileRecurringPayments.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ProfileRPSuccess from './ProfileRPSuccess.jsx';
+import ProfileSetRP from './ProfileSetRP.jsx';
+
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,62 +33,29 @@ const ProfileRecurringPayments = props => {
   }
 
   return (
-    <Paper style={{ margin: 20, padding: 15 }}>
-      <Grid container direction="column" justify="center" alignItems="center">
-        <Typography variant="h4">Set a payment for every month!</Typography>
-        <Typography>Day of month:</Typography>
-        <Select value={props.inputDay} onChange={props.handleDayChange}>
-          {days.map(day => {
-            return (
-              <MenuItem key={day} value={day}>
-                {day}
-              </MenuItem>
-            );
-          })}
-        </Select>
-
-        <TextField
-          id="amount"
-          label="amount"
-          type="number"
-          onChange={props.handleInputAmount}
-          margin="normal"
-        />
-        <Typography>Budget Category</Typography>
-        <Select
-          value={props.inputCategory}
-          onChange={props.handleCategoryInput}
-        >
-          {props.categories.map((category, i) => {
-            return (
-              <MenuItem key={`categoryInput_${i}`} value={category.name}>
-                {category.name}{' '}
-              </MenuItem>
-            );
-          })}
-        </Select>
-        <Typography>Account:</Typography>
-        <Select value={props.inputAccount} onChange={props.handleAccountInput}>
-          {props.accounts.map((account, i) => {
-            return (
-              <MenuItem key={`accountInput_${i}`} value={account.name}>
-                {account.name}
-              </MenuItem>
-            );
-          })}
-        </Select>
-        <Typography>Payee:</Typography>
-        <TextField
-          id="payee"
-          label="payee"
-          onChange={props.handlePayeeInput}
-          margin="normal"
-        />
-        <Button onClick={props.handleRecurringPayment} color="primary">
-          Add monthly payment
-        </Button>
-      </Grid>
-    </Paper>
+    <div>
+      {props.showSuccessMessage ? (
+        <Paper className={classes.paper}>
+          <ProfileRPSuccess toggleSuccessMessage={props.toggleSuccessMessage} />
+        </Paper>
+      ) : (
+        <Paper style={{ margin: 20, padding: 15 }}>
+          <ProfileSetRP
+            inputDay={props.inputDay}
+            handleDayChange={props.handleDayChange}
+            days={days}
+            handleInputAmount={props.handleInputAmount}
+            inputCategory={props.inputCategory}
+            categories={props.categories}
+            inputAccount={props.inputAccount}
+            handleAccountInput={props.handleAccountInput}
+            handlePayeeInput={props.handlePayeeInput}
+            handleRecurringPayment={props.handleRecurringPayment}
+            accounts={props.accounts}
+          ></ProfileSetRP>
+        </Paper>
+      )}
+    </div>
   );
 };
 
