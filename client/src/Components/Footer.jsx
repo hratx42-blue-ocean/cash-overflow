7dd42125a4807e1698118d6c3e8d4dc64678aa4a
Evelyn Binkard <evelyn.binkard@gmail.com>
2019-08-16 18:14:41 -0500

    added modal to footer

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import { useAuth0 } from '../react-auth0-wrapper';
-import { mergeClasses } from '@material-ui/styles';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+const engineers = [
+  'Garett Welson',
+  'Ross Calimlin',
+  'Doris Hui',
+  'Kevin Bench',
+  'Evelyn Binkard',
+  'Mitchell Dill',
+  'Jordan Dilliard',
+  'Brandon Leafman'
+];
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +35,7 @@ const useStyles = makeStyles(theme => ({
     textDecoration: 'none',
     color: '#ffffff',
     padding: 0,
-    margin: 2,
+    margin: 2
   },
   toolbar: {
     position: 'fixed',
@@ -34,20 +44,42 @@ const useStyles = makeStyles(theme => ({
     right: 10,
     bottom: 4,
     minHeight: 10
-    
-    
-
-  },
+  }
 }));
 
 export default function Footer() {
-    const classes = useStyles();
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
   return (
     <div className={classes.root}>
       <Toolbar height={4} width={1} className={classes.toolbar}>
-       
-          <Button className={classes.link}>About Us</Button>
-        
+        <Button onClick={handleClickOpen} className={classes.link}>
+          About Us
+        </Button>
+        <Dialog aria-labelledby="simple-dialog-title" open={open}>
+          <DialogTitle>Team Cash Overflow</DialogTitle>
+          <List>
+            {engineers.map(engineer => (
+              <ListItem key={engineer}>
+                <ListItemText primary={engineer} />
+              </ListItem>
+            ))}
+          </List>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Toolbar>
     </div>
   );
