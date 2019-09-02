8dd2bf02187cf961be121ff0351ba725a66ee12c
Ross Calimlim <ross.calimlim@gmail.com>
2019-09-01 21:19:14 -0500

    Refactor Dashboard to be functional

--- client/src/Components/DashboardPage.jsx
+++ client/src/Components/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
@@ -18,124 +18,98 @@ import {
   MuiPickersUtilsProvider
 } from '@material-ui/pickers';
 import PropTypes from 'prop-types';
-import { positions } from '@material-ui/system';
-import Loading from './Loading.jsx';
-import AlertBox from './AlertBox.jsx';
-import { Auth0Context } from '../react-auth0-wrapper';
+import { makeStyles } from '@material-ui/styles';
+import Loading from './Loading';
+// import { Auth0Context } from '../react-auth0-wrapper';
 
 import db from '../utils/databaseRequests';
 import format from '../utils/formatCurrency';
 
-const styles = {
+const useStyles = makeStyles({
   root: {
     flexGrow: 1
   }
-};
+});
+const DashboardPage = ({
+  user,
+  accounts,
+  categories,
+  transactions,
+  loading,
+  isAuthenticated
+}) => {
+  // state
+  const [txAccount, setTxAccount] = useState('');
+  const [txAccountId, setTxAccountId] = useState(undefined);
+  const [txAmount, setTxAmount] = useState('');
+  const [txCategory, setTxCategory] = useState('');
+  const [txCategoryId, setTxCategoryId] = useState(undefined);
+  const [txDate, setTxDate] = useState(moment().format('YYYY-MM-DD'));
+  const [txMemo, setTxMemo] = useState('');
+  const [txType, setTxType] = useState('');
 
-export default class DashboardPage extends Component {
-  constructor(props) {
-    super(props);
-    this.user = props.user;
-    this.accounts = props.accounts;
-    this.categories = props.categories;
-    this.transactions = props.transactions;
+  // styling
+  const classes = useStyles();
 
-    this.state = {
-      txAccount: '',
-      txAccountId: undefined,
-      txAmount: undefined,
-      txCategory: undefined,
-      txCategoryId: '',
-      txDate: moment().format('YYYY-MM-DD'),
-      txMemo: '',
-      txType: ''
-    };
-    this.handleAccountInput = this.handleAccountInput.bind(this);
-    this.handleAmountInput = this.handleAmountInput.bind(this);
-    this.handleCategoryInput = this.handleCategoryInput.bind(this);
-    this.handleDateInput = this.handleDateInput.bind(this);
-    this.handleMemoInput = this.handleMemoInput.bind(this);
-    this.handleTransactionType = this.handleTransactionType.bind(this);
-    this.handleSubmitTransaction = this.handleSubmitTransaction.bind(this);
-  }
+  const handleAccountInput = event => {
+    setTxAccount(event.target.value);
+    setTxAccountId(event.nativeEvent.target.getAttribute('recordid'));
+  };
 
-  handleAccountInput(event) {
-    const txAccount = event.target.value;
-    const txAccountId = Number(
-      event.nativeEvent.target.getAttribute('recordid')
-    );
-    this.setState({ txAccount, txAccountId });
-  }
+  const handleAmountInput = event => {
+    setTxAmount(Number(event.target.value));
+  };
 
-  handleAmountInput(event) {
-    const txAmount = Number(event.target.value);
-    this.setState({ txAmount });
-  }
+  const handleCategoryInput = event => {
+    setTxCategory(event.target.value);
+    setTxCategoryId(event.nativeEvent.target.getAttribute('recordid'));
+  };
 
-  handleCategoryInput(event) {
-    const txCategory = event.target.value;
-    const txCategoryId = Number(
-      event.nativeEvent.target.getAttribute('recordid')
-    );
-    this.setState({ txCategory, txCategoryId });
-  }
+  const handleDateInput = event => {
+    setTxDate(event.format('YYYY-MM-DD'));
+  };
 
-  handleDateInput(event) {
-    this.setState({ txDate: event.format('YYYY-MM-DD') });
-  }
+  const handleMemoInput = event => {
+    setTxMemo(event.target.value);
+  };
 
-  handleMemoInput(event) {
-    const txMemo = event.target.value;
-    this.setState({ txMemo });
-  }
-
-  handleTransactionType(event) {
-    const txType = event.target.value;
-    if (txType === 'outflow') {
+  const handleTransactionType = event => {
+    if (event.target.value === 'outflow') {
       // debit
-      this.setState({ txType: 1 });
+      setTxType(1);
     } else {
       // credit
-      this.setState({ txType: 2 });
+      setTxType(2);
     }
-  }
+  };
 
-  clearTransactionInput() {
-    this.setState({
-      txAccount: '',
-      txAccountId: undefined,
-      txAmount: undefined,
-      txCategory: undefined,
-      txCategoryId: '',
-      txDate: moment(),
-      txMemo: '',
-      txType: ''
-    });
-  }
-
-  calculateTotalBalance() {
-    return this.accounts.reduce((sum, account) => {
+  const calculateTotalBalance = () => {
+    return accounts.reduce((sum, account) => {
       // only adds balances for accounts of type checking or savings
       if (account.type !== 3) {
         return sum + account.balance;
       }
       return sum;
     }, 0);
-  }
+  };
 
-  handleSubmitTransaction() {
-    const {
-      txAccountId,
-      txAmount,
-      txCategoryId,
-      txDate,
-      txMemo,
-      txType
-    } = this.state;
+  const clearTransactionInput = () => {
+    setState({
+      txAccount: '',
+      txAccountId: undefined,
+      txAmount: '',
+      txCategory: '',
+      txCategoryId: undefined,
+      txDate: moment(),
+      txMemo: '',
+      txType: ''
+    });
+  };
+
+  const handleSubmitTransaction = () => {
     const txRecurring = 0;
-    const txUser = this.user.id;
+    const txUser = user.id;
 
-    console.log('state is', this.state);
     db.postTransaction(
       txAccountId,
       txAmount,
@@ -146,154 +120,144 @@ export default class DashboardPage extends Component {
       txType,
       txUser
     )
-      .then(console.log)
+      .then(() => clearTransactionInput())
       .catch(console.error);
-  }
+  };
 
-  render() {
-    const { loading, isAuthenticated } = this.props;
-
-    if (loading || !isAuthenticated) {
-      return (
-        <div data-testid="auth-loading">
-          <Loading />
-        </div>
-      );
-    }
-
+  if (loading || !isAuthenticated) {
     return (
-      <div>
-        <Grid
-          container
-          direction="row"
-          justify="center"
-          alignItems="flex-start"
-          style={{ padding: 20 }}
-        >
-          <Grid
-            container
-            direction="column"
-            justify="space-between"
-            alignItems="flex-start"
-            style={{ width: '40%', margin: 20 }}
-          >
-            <Paper
-              style={{
-                width: '100%',
-                height: 150,
-                marginBottom: 20,
-                padding: 25
-              }}
-            >
-              <Typography
-                style={{ textAlign: 'center' }}
-                variant="h3"
-                gutterBottom
-              >
-                Hello, {this.user.first_name}!
-                <Tooltip
-                  placement="top"
-                  title="Safe to spend balance: bank accounts less credit card debt"
-                >
-                  <Typography style={{ textAlign: 'center' }} variant="h5">
-                    You have {format(this.calculateTotalBalance())} total
-                  </Typography>
-                </Tooltip>
-              </Typography>
-            </Paper>
-          </Grid>
-
-          <Paper style={{ width: '40%', margin: 20, padding: 25 }}>
-            <Grid
-              container
-              direction="column"
-              justify="center"
-              alignItems="center"
-            >
-              <Typography variant="h4">Add a transaction</Typography>
-              <MuiPickersUtilsProvider utils={MomentUtils}>
-                <KeyboardDatePicker
-                  variant="inline"
-                  format="MM/DD/YYYY"
-                  margin="normal"
-                  value={this.state.txDate}
-                  onChange={this.handleDateInput}
-                />
-              </MuiPickersUtilsProvider>
-
-              <TextField
-                id="amount"
-                label="amount"
-                type="number"
-                value={this.state.txAmount}
-                onChange={this.handleAmountInput}
-                margin="normal"
-              />
-              <FormLabel component="legend">
-                Is this an outflow or inflow?{' '}
-              </FormLabel>
-              <RadioGroup
-                aria-label="position"
-                name="position"
-                onChange={this.handleTransactionType}
-                row
-              >
-                <FormControlLabel
-                  value="outflow"
-                  control={<Radio color="primary" />}
-                  label="outflow"
-                  labelPlacement="start"
-                />
-                <FormControlLabel
-                  value="inflow"
-                  control={<Radio color="primary" />}
-                  label="inflow"
-                  labelPlacement="start"
-                />
-              </RadioGroup>
-              <Select
-                value={this.state.txCategory}
-                onChange={this.handleCategoryInput}
-              >
-                {this.categories.map(category => {
-                  const { id, name } = category;
-                  return (
-                    <MenuItem key={id} recordid={id} value={name}>
-                      {name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-              <Select
-                value={this.state.txAccount}
-                onChange={this.handleAccountInput}
-              >
-                {this.accounts.map(account => {
-                  const { id, name } = account;
-                  return (
-                    <MenuItem key={id} recordid={id} value={name}>
-                      {name}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-              <TextField
-                id="payee"
-                label="payee"
-                value={this.state.txMemo}
-                onChange={this.handleMemoInput}
-                margin="normal"
-              />
-              <Button onClick={this.handleSubmitTransaction} color="primary">
-                Add transaction
-              </Button>
-            </Grid>
-          </Paper>
-        </Grid>
+      <div data-testid="auth-loading">
+        <Loading />
       </div>
     );
   }
-}
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="flex-start"
+        style={{ padding: 20 }}
+      >
+        <Grid
+          container
+          direction="column"
+          justify="space-between"
+          alignItems="flex-start"
+          style={{ width: '40%', margin: 20 }}
+        >
+          <Paper
+            style={{
+              width: '100%',
+              height: 150,
+              marginBottom: 20,
+              padding: 25
+            }}
+          >
+            <Typography
+              style={{ textAlign: 'center' }}
+              variant="h3"
+              gutterBottom
+            >
+              Hello, {user.first_name}!
+              <Tooltip
+                placement="top"
+                title="Safe to spend balance: bank accounts less credit card debt"
+              >
+                <Typography style={{ textAlign: 'center' }} variant="h5">
+                  You have {format(calculateTotalBalance())} total
+                </Typography>
+              </Tooltip>
+            </Typography>
+          </Paper>
+        </Grid>
+
+        <Paper style={{ width: '40%', margin: 20, padding: 25 }}>
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Typography variant="h4">Add a transaction</Typography>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <KeyboardDatePicker
+                variant="inline"
+                format="MM/DD/YYYY"
+                margin="normal"
+                value={txDate}
+                onChange={handleDateInput}
+              />
+            </MuiPickersUtilsProvider>
+
+            <TextField
+              id="amount"
+              label="amount"
+              type="number"
+              value={txAmount}
+              onChange={handleAmountInput}
+              margin="normal"
+            />
+            <FormLabel component="legend">
+              Is this an outflow or inflow?{' '}
+            </FormLabel>
+            <RadioGroup
+              aria-label="position"
+              name="position"
+              onChange={handleTransactionType}
+              row
+            >
+              <FormControlLabel
+                value="outflow"
+                control={<Radio color="primary" />}
+                label="outflow"
+                labelPlacement="start"
+              />
+              <FormControlLabel
+                value="inflow"
+                control={<Radio color="primary" />}
+                label="inflow"
+                labelPlacement="start"
+              />
+            </RadioGroup>
+            <Select value={txCategory} onChange={handleCategoryInput}>
+              {categories.map(category => {
+                const { id, name } = category;
+                return (
+                  <MenuItem key={id} recordid={id} value={name}>
+                    {name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            <Select value={txAccount} onChange={handleAccountInput}>
+              {accounts.map(account => {
+                const { id, name } = account;
+                return (
+                  <MenuItem key={id} recordid={id} value={name}>
+                    {name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+            <TextField
+              id="payee"
+              label="payee"
+              value={txMemo}
+              onChange={handleMemoInput}
+              margin="normal"
+            />
+            <Button onClick={handleSubmitTransaction} color="primary">
+              Add transaction
+            </Button>
+          </Grid>
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
 
 DashboardPage.defaultProps = {
   accountData: {
@@ -311,8 +275,4 @@ DashboardPage.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired
 };
 
-DashboardPage.contextType = {
-  Auth0Context: PropTypes.object.isRequired
-};
-
-DashboardPage.contextType = Auth0Context;
+export default DashboardPage;
